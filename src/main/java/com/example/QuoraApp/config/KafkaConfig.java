5355ebfaf2fcd2095bfee1014fc2a48c05d20a2b
Krityan Kashyap <krityankashyap@Krityans-MacBook-Air.local>
2026-06-24 13:50:06 +0530

    Added consumers

--- src/main/java/com/example/QuoraApp/config/KafkaConfig.java
+++ src/main/java/com/example/QuoraApp/config/KafkaConfig.java
@@ -3,12 +3,15 @@ package com.example.QuoraApp.config;
 import java.util.HashMap;
 import java.util.Map;
 
+import org.apache.kafka.clients.consumer.ConsumerConfig;
 import org.apache.kafka.clients.producer.ProducerConfig;
 import org.apache.kafka.common.serialization.StringSerializer;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.kafka.annotation.EnableKafka;
+import org.springframework.kafka.core.ConsumerFactory;
+import org.springframework.kafka.core.DefaultKafkaConsumerFactory;
 import org.springframework.kafka.core.DefaultKafkaProducerFactory;
 import org.springframework.kafka.core.ProducerFactory;
 
@@ -32,5 +35,16 @@ public class KafkaConfig {
     configProcess.put(ProducerConfig.VALUE_SERIALIZER_CLASS_CONFIG, StringSerializer.class);
 
     return new DefaultKafkaProducerFactory<>(configProcess);
+  }
+
+  @Bean
+  public ConsumerFactory<String, Object> consumerFactory(){
+    Map<String, Object> configProcess= new HashMap<>();
+    configProcess.put(ConsumerConfig.BOOTSTRAP_SERVERS_CONFIG, bootstrapServer);
+    configProcess.put(ConsumerConfig.GROUP_ID_CONFIG, groupId);
+    configProcess.put(ConsumerConfig.KEY_DESERIALIZER_CLASS_CONFIG, StringSerializer.class);
+    configProcess.put(ConsumerConfig.VALUE_DESERIALIZER_CLASS_CONFIG, StringSerializer.class);
+
+    return new DefaultKafkaConsumerFactory<>(configProcess);
   }
 }
